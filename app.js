5340ed0485ccd74ec4095b49ac03c566dd648d32
KaarelKallas <71066613+KaarelKallas@users.noreply.github.com>
2021-02-13 17:18:46 +0200

    3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var player = document.getElementById("player");
 var object = document.getElementById("object");
 object.classList.add("obAn1");
 
+
 function jump() {
     if (player.classList != "animate") {
         player.classList.add("animate");
@@ -17,8 +18,13 @@ var checkDead = setInterval(function () {
     var scoreInt = document.getElementById("scoreInt").innerHTML;
     var score = 0
 
+
     if (objectRight < 80 && objectRight > 0 && playerBottom <= 10) {
         var ded = true
+        var highScore = document.getElementById("hs").innerHTML;
+        if (parseInt(highScore) < parseInt(scoreInt)) {
+            localStorage.highScore = parseInt(scoreInt)
+        }
 
         document.getElementById("scoreInt").innerHTML = 0
         object.classList.remove("obAn2");
@@ -30,23 +36,27 @@ var checkDead = setInterval(function () {
     score++
     if (ded != true) {
         document.getElementById("scoreInt").innerHTML++
+
     }
-    if (scoreInt > 1000) {
+
+
+    if (scoreInt > 1000 && objectRight < 10) {
         object.classList.remove("obAn1");
         object.classList.add("obAn2");
     }
-    if (scoreInt > 2000) {
+    if (scoreInt > 2000 && objectRight < 10) {
         object.classList.remove("obAn2");
         object.classList.add("obAn3");
     }
-    if (scoreInt > 4000) {
+    if (scoreInt > 4000 && objectRight < 10) {
         object.classList.remove("obAn3");
         object.classList.add("obAn4");
     }
-    if (scoreInt > 6000) {
+    if (scoreInt > 6000 && objectRight < 10) {
         object.classList.remove("obAn4");
         object.classList.add("obAn5");
     }
 
 
+    document.getElementById("hs").innerHTML = localStorage.highScore
 }, 10);
